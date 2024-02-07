a2f073e4e91b5f4352884e02db3daa9a88ca6730
NLH-Gnext <gnext.nyanlinhtet@gmail.com>
2024-02-07 15:09:23 +0630

    drawer added

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
+// import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{ title: "All Categories" }}
+      />
+      <Drawer.Screen name="Favorite" component={FavoriteScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-
   return (
     <>
       <StatusBar style="light" />
@@ -21,15 +45,16 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen
             name="MealsOverview"
             component={MealsOverviewScreen}
+
             // options={({ route, navigation }) => {
             //   const catId = route.params.categoryId;
             //   return {
@@ -37,7 +62,11 @@ export default function App() {
             //   };
             // }}
           />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+          <Stack.Screen
+            name="MealDetail"
+            component={MealDetailScreen}
+            options={{ title: "About the meal" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const FavoriteScreen = () => {
+  return (
+    <View>
+      <Text>Fav Screen</Text>
+    </View>
+  );
+};
+
+export default FavoriteScreen;
